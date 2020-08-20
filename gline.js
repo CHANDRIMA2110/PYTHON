259f38fab59fa10e126111b9c3257ee12492d8a3
CHANDRIMA2110 <69992742+CHANDRIMA2110@users.noreply.github.com>
2020-08-21 04:18:50 +0530

    Add files via upload

---
+++ gline.js
@@ -0,0 +1,5 @@
+gline = [ ['Month','umich.edu','berkeley.edu','ucdavis.edu','uct.ac.za','ufp.pt','columbia.edu','gmail.com','virginia.edu','northwestern.edu','cam.ac.uk'],
+['2005-12',69,12,11,14,10,13,10,9,16,7],
+['2006-01',125,33,37,27,32,28,23,21,12,19],
+['2006-02',36,6,1,5,3,3,4,6,6,6]
+];
